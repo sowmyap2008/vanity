039efa6eae7f986a2c0340a7d342c6e1ebf54555
sowmyap2008 <psowmya2008@gmail.com>
2026-08-25 09:27:47 +0530

    Update caching logs in service worker

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Caching assets...');
+        console.log('Vanity: Caching assets...');
         return cache.addAll(urlsToCache);
       })
   );
@@ -20,7 +20,6 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Return cached version or fetch from network
         return response || fetch(event.request);
       })
   );
@@ -33,7 +32,7 @@ self.addEventListener('activate', event => {
       return Promise.all(
         cacheNames.map(cacheName => {
           if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
+            console.log('Vanity: Deleting old cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
